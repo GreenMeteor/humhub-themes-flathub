ab4c405ab403999d5b33bdc6a597829a924572a4
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-08-07 21:18:00 -0700

    Update likeLink.php

--- views/like/widgets/likeLink.php
+++ views/like/widgets/likeLink.php
@@ -1,42 +1,31 @@
 <?php
 
-use yii\helper\Html;
+use yii\helpers\Html;
 
 humhub\modules\like\assets\LikeAsset::register($this);
 ?>
 
-<span class="likeLinkContainer pull-left" id="likeLinkContainer_<?= $id ?>">
+<span class="likeLinkContainer" id="likeLinkContainer_<?= $id ?>">
 
     <?php if (Yii::$app->user->isGuest): ?>
-        <?= Html::a(Yii::t('LikeModule.widgets_views_likeLink', '<i class="fa fa-heart-o"></i>'), Yii::$app->user->loginUrl, array('data-target' => '#globalModal')); ?>
-    <?php else {
-    : ?>
-        <?= Html::a(Yii::t('LikeModule.widgets_views_likeLink', '<i class="fa fa-thumbs-o-up"></i>'), $likeUrl, ['style' => 'display:'.((!$currentUserLiked) ? 'inline' : 'none'), 'class' => 'like likeAnchor', 'data-objectId' => $id]);
-}
-?>
-        <?= Html::a(Yii::t('LikeModule.widgets_views_likeLink', '<i class="fa fa-thumbs-o-up"></i>'), $unlikeUrl, ['style' => 'display:'.(($currentUserLiked) ? 'inline' : 'none'), 'class' => 'unlike likeAnchor', 'data-objectId' => $id]); ?>
+
+        <?php echo Html::a(Yii::t('LikeModule.widgets_views_likeLink', 'Like'), Yii::$app->user->loginUrl, ['data-target' => '#globalModal']); ?>
+    <?php else: ?>
+        <a href="#" data-action-click="like.toggleLike" data-action-url="<?= $likeUrl ?>" class="like likeAnchor" style="<?= (!$currentUserLiked) ? '' : 'display:none'?>">
+            <?= Yii::t('LikeModule.widgets_views_likeLink', 'Like') ?>
+        </a>
+        <a href="#" data-action-click="like.toggleLike" data-action-url="<?= $unlikeUrl ?>" class="unlike likeAnchor" style="<?= ($currentUserLiked) ? '' : 'display:none'?>">
+            <?= Yii::t('LikeModule.widgets_views_likeLink', 'Unlike') ?>
+        </a>
     <?php endif; ?>
 
-<?php if (count($likes) > 0) { ?>
+    <?php if (count($likes) > 0) { ?>
         <!-- Create link to show all users, who liked this -->
-        <a href="<?= $userListUrl; ?>" data-target="#globalModal"><span class="likeCount tt" data-placement="top" data-toggle="tooltip"
-                                                    title="<?= $title ?>"></span></a>
+        <a href="<?php echo $userListUrl; ?>" data-target="#globalModal">
+            <span class="likeCount tt" data-placement="top" data-toggle="tooltip" title="<?= $title ?>">(<?= count($likes) ?>)</span>
+        </a>
     <?php } else { ?>
         <span class="likeCount"></span>
-<?php } ?>
+    <?php } ?>
 
 </span>
-
-<script>
-    $(function () {
-        updateLikeCounters($("#likeLinkContainer_<?= $id ?>"), <?= count($likes); ?>);
-        initLikeModule();
-
-        // show Tooltips on elements inside the views, which have the class 'tt'
-        $('.tt').tooltip({
-            html: false,
-            container: 'body'
-        });
-
-    });
-</script>
