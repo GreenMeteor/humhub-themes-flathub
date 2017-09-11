8bd7a0fa9fbbf2969333f687a0ee4eb69ac2592a
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-09-10 20:40:20 -0700

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -98,7 +98,9 @@ AppAsset::register($this);
     <?php if (isset($this->context->subLayout) && $this->context->subLayout != "") : ?>
         <?= $this->render($this->context->subLayout, array('content' => $content)); ?>
     <?php else: ?>
+
         <?= $content; ?>
+
     <?php endif; ?>
     <!-- end: show content -->
 
