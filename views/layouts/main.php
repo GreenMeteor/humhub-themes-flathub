61a2eb0cdb7c3e770f565bc171abe5e3daec44c2
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-06 16:55:17 +0900

    Upgrade: 1.2 compatibility
    With this commit, it should be compatible with `v1.2` now! The only thing needed now is to fix the missing top-nav.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,82 +1,46 @@
 <?php
 
-use yii\helpers\Html;
-use humhub\assets\AppAsset;
-
 /* @var $this \yii\web\View */
 /* @var $content string */
-
-AppAsset::register($this);
+\humhub\assets\AppAsset::register($this);
 ?>
 
 <?php $this->beginPage() ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head> 
-        <!-- start: Meta -->
-        <meta charset="utf-8">
-        <title><?= $this->pageTitle; ?></title>
-        <!-- end: Meta -->
-
-        <!-- start: Mobile Specific -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-        <!-- end: Mobile Specific -->
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <title><?= strip_tags($this->pageTitle); ?></title>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <?php $this->head() ?>
-
-        <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-        <script src="<?= Yii::getAlias(" @web"); ?>/js/html5shiv.js"></script>
-        <link id="ie-style" href="<?= Yii::getAlias("@web"); ?>/css/ie.css" rel = "stylesheet" >
-        <![endif]-->
-
-        <!--[if IE 9]>
-        <link id="ie9style" href="<?= Yii::getAlias(" @web"); ?>/css/ie9.css" rel="stylesheet">
-        <![endif]-->
-
-        <!-- start: render additional head (css and js files) -->
         <?= $this->render('head'); ?>
-        <!-- end: render additional head -->
-
     </head>
-
     <body>
-	
-	<script src="<?= $this->theme->getBaseUrl() . '/js/lightbox-plus-jquery.min.js'; ?>"></script>
-	
-    <?php $this->beginBody() ?>
-	
-    <!-- start: first top navigation bar -->
-    <div id="topbar-first" class="topbar">
-        <div class="container">
+        <?php $this->beginBody() ?>
 
-            <div class="topbar-brand hidden-xs">
-                <?= \humhub\widgets\SiteLogo::widget(); ?>
-            </div>
+        <!-- start: first top navigation bar -->
+        <div id="topbar-first" class="topbar">
+            <div class="container">
+                <div class="topbar-brand hidden-xs">
+                    <?= \humhub\widgets\SiteLogo::widget(); ?>
+                </div>
 
-            <div class="topbar-actions pull-right">
-				<div class="no-icons">
-					<?= \humhub\modules\user\widgets\AccountTopMenu::widget(); ?>
-				</div>
-				<form id="switchform">
+                <div class="topbar-actions pull-right">
+                    <?= \humhub\modules\user\widgets\AccountTopMenu::widget(); ?>
+                    <form id="switchform">
 					<a href="javascript:chooseStyle('none', 60)" checked="checked"><i class="fa fa-sun-o"></i></a>
 					<a href="javascript:chooseStyle('dark-theme', 60)"<i class="fa fa-moon-o"></i></a>
 				</form>
-            </div>
+                </div>
 
-            <div class="notifications pull-right">
-                <?=
-	\humhub\widgets\NotificationArea::widget(['widgets' => [
-                    [\humhub\modules\notification\widgets\Overview::className(), [], ['sortOrder' => 10]],
-                ]]);
-                ?>
+                <div class="notifications pull-right">
+                    <?= \humhub\widgets\NotificationArea::widget(); ?>
+                </div>
             </div>
         </div>
-    </div>
-    
-    <!-- end: first top navigation bar -->
+        <!-- end: first top navigation bar -->
 
-    <!-- start: second top navigation bar -->
+        <!-- start: second top navigation bar -->
         <div id="topbar-second" class="topbar">
             <div class="container">
                 <ul class="nav" id="top-menu-nav">
@@ -84,7 +48,7 @@ AppAsset::register($this);
                     <?= \humhub\modules\space\widgets\Chooser::widget(); ?>
 
                     <!-- load navigation from widget -->
-                  <?= \humhub\widgets\TopMenu::widget(); ?>
+                    <?= \humhub\widgets\TopMenu::widget(); ?>
                 </ul>
 
                 <ul class="nav pull-right" id="search-menu-nav">
@@ -93,56 +57,21 @@ AppAsset::register($this);
             </div>
         </div>
         <!-- end: second top navigation bar -->
-
-    <?= \humhub\modules\tour\widgets\Tour::widget(); ?>
-
-    <!-- start: show content (and check, if exists a sublayout -->
-    <?php if (isset($this->context->subLayout) && $this->context->subLayout != "") : ?>
-        <?= $this->render($this->context->subLayout, array('content' => $content)); ?>
-    <?php else: ?>
-        <?= $content; ?>
-    <?php endif; ?>
-    <!-- end: show content -->
-
-    <!-- start: Modal (every lightbox will/should use this construct to show content)-->
-    <div class="modal" id="globalModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <?= \humhub\widgets\LoaderWidget::widget(); ?>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- end: Modal -->
-
-    <?= \humhub\models\Setting::GetText('trackingHtmlCode'); ?>
-    <?php $this->endBody() ?>
-
-	<div class="container">
-		<div class="col-md-12 layout-content-container">
-			<footer>
-				<div class="copyright">
-					<span>Powered by <a target="_blank" href="http://humhub.com">HumHub</a></span>
-					<span>Theme <a target="_blank" href="http://thrashed.ru">developer</a></span>
-				</div>
-			</footer>
-		</div>
+        
+        <div class="container">		
+		<div class="col-md-12 layout-content-container">		
+			<footer>		
+				<div class="copyright">		
+					<span>Powered by <a target="_blank" href="https://humhub.org/">HumHub</a></span>		
+					<span>Theme <a target="_blank" href="http://thrashed.ru">developer</a></span>		
+				</div>		
+			</footer>		
+		</div>		
 	</div>
 
-	<script src="<?= $this->theme->getBaseUrl() . '/js/plugins-scroll.js'; ?>"></script>
-	<script>
-	$(document).ready(function() {
-		try {
-			$.browserSelector();
-			if($("html").hasClass("chrome")) {
-				$.smoothScroll();
-			}
-		} catch(err) {
-		};
-	});
-	</script>
+        <?= $content; ?>
 
+        <?php $this->endBody() ?>
     </body>
-    </html>
+</html>
 <?php $this->endPage() ?>
